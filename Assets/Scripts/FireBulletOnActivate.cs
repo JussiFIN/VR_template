322413b6390ae3ff675093bece9d1b63239cb846
JussiFIN <56635784+JussiFIN@users.noreply.github.com>
2022-10-25 18:21:42 +0300

    Added object interaction using XR Toolkit and a pistol
    Cube with black block uses Instantaneous movement type, blue uses kinematic and white uses Velocity. Notice the difference.
    Pistol shoots bullets with trigger.

---
+++ Assets/Scripts/FireBulletOnActivate.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class FireBulletOnActivate : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform spawnPos;
+    public float speed = 20f;
+
+    void Start()
+    {
+        XRGrabInteractable grabbable = GetComponent<XRGrabInteractable>();
+        grabbable.activated.AddListener(FireBullet);
+    }
+
+    public void FireBullet(ActivateEventArgs arg)
+    {
+        GameObject spawnedBullet = Instantiate(bullet);     //tehdään luoti GameObject, "poolaa" tämä ettei sitä aina tehdä uudestaan
+        spawnedBullet.transform.position = spawnPos.position;   //asetetaan sen position
+        spawnedBullet.transform.rotation = Quaternion.LookRotation(spawnPos.forward, spawnPos.up);  //asetetaan myös rotaatio jota ei ollu tutoriaalissa
+        spawnedBullet.GetComponent<Rigidbody>().velocity = spawnPos.forward * speed;    //annetaan vauhtia
+        Destroy(spawnedBullet, 3f);     //tuhotaan luoti 3 sekunnin kuluttua
+    }
+}
---
+++ Assets/Scripts/XRGrapInteractableToAttach.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class XRGrapInteractableToAttach : XRGrabInteractable
+{
+
+    protected override void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        base.OnSelectEntered(args);
+    }
+}
